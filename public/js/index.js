0868def163d4c7353226af050d01466766d24281
三浦昌樹 <miura@miuramasakinoMacBook-Pro.local>
2018-11-09 22:44:20 +0900

    locate

--- public/js/index.js
+++ public/js/index.js
@@ -8,28 +8,37 @@ socket.on('disconnect', function () {
   console.log('Disconnected from server');
 });
 
-// server から受信
 socket.on('newMessage', function (message) {
   console.log('newMessage', message);
-
-  //パラグラフ作成
   var li = jQuery('<li></li>');
   li.text(`${message.from}: ${message.text}`);
 
-  // HTML のid messages　に li 追加される
   jQuery('#messages').append(li);
 });
-
 
 jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
 
-  // 
   socket.emit('createMessage', {
     from: 'User',
-    // message に入力された文字
     text: jQuery('[name=message]').val()
   }, function () {
 
+  });
+});
+
+var locationButton = jQuery('#send-location');
+locationButton.on('click', function () {
+  if (!navigator.geolocation) {
+    return alert('Geolocation not supported by your browser.');
+  }
+
+  navigator.geolocation.getCurrentPosition(function (position) {
+    socket.emit('createLocationMessage', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    });
+  }, function () {
+    alert('Unable to fetch location.');
   });
 });
