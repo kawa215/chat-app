de9bd456bb0dec579fd25bda918e0a3d13f5e20f
三浦昌樹 <miura@miuramasakinoMacBook-Pro.local>
2018-11-09 20:50:40 +0900

    Add form

--- public/js/index.js
+++ public/js/index.js
@@ -8,19 +8,26 @@ socket.on('disconnect', function () {
   console.log('Disconnected from server');
 });
 
+// server から受信
 socket.on('newMessage', function (message) {
   console.log('newMessage', message);
+
+  //パラグラフ作成
   var li = jQuery('<li></li>');
   li.text(`${message.from}: ${message.text}`);
 
+  // HTML のid messages　に li 追加される
   jQuery('#messages').append(li);
 });
+
 
 jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
 
+  // 
   socket.emit('createMessage', {
     from: 'User',
+    // message に入力された文字
     text: jQuery('[name=message]').val()
   }, function () {
 
